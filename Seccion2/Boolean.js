97845343ab9767bf6280d45a9d4c9493487d399d
Rosinet <christian.romero@rosinet.com.mx>
2020-01-25 23:46:01 -0600

    Declaracion de variable Tipado

---
+++ Seccion2/Boolean.js
@@ -0,0 +1,9 @@
+var soy_alto = true;
+var soy_fan = false;
+// soy_alto=20;
+console.log("Soy alto ", soy_alto);
+console.log("Soy fan ", soy_fan);
+// Operador de conjuncion (y)
+console.log(soy_alto && soy_fan);
+// Operador de disyuncion (o)
+console.log(soy_alto || soy_fan);
---
+++ Seccion2/Boolean.ts
@@ -0,0 +1,12 @@
+let soy_alto: boolean=true;
+let soy_fan: boolean=false;
+
+// soy_alto=20;
+console.log("Soy alto ", soy_alto );
+console.log("Soy fan ", soy_fan);
+// Operador de conjuncion (y)
+console.log(soy_alto && soy_fan);
+// Operador de disyuncion (o)
+console.log(soy_alto || soy_fan);
+
+
