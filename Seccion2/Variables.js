4b70c3241b6bbb7b9b4948c9a102e61b255ad165
Rosinet <christian.romero@rosinet.com.mx>
2020-01-25 23:31:09 -0600

    Seccion 2 declaracion de variables

---
+++ Seccion2/Variables.js
@@ -0,0 +1,8 @@
+// Declarar
+var nombre_de_mi_variable;
+// Inicializar
+nombre_de_mi_variable = true;
+// Declarar e inicializar
+var equipo = "Necaxa";
+// Declarar constantes
+var pi = 3.1417;
---
+++ Seccion2/Variables.ts
@@ -0,0 +1,11 @@
+// Declarar
+let nombre_de_mi_variable;
+
+// Inicializar
+nombre_de_mi_variable=true;
+
+// Declarar e inicializar
+let equipo="Necaxa";
+
+// Declarar constantes
+const pi=3.1417;
