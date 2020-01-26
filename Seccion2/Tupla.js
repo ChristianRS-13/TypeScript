21faecc5576f918aba4c0a8b44ff9049865e0310
Rosinet <christian.romero@rosinet.com.mx>
2020-01-26 13:14:25 -0600

    Manejo de Tuplas

---
+++ Seccion2/Tupla.js
@@ -0,0 +1,10 @@
+var vector;
+var vector2;
+vector = [2, 5];
+vector2 = [5, 10];
+var vector_resultante = [vector[0] + vector2[0], vector[1] + vector2[1]];
+console.log(vector_resultante);
+var tupla_5_elementos = [1, 2, 3, 4, 5];
+var tupla_profesor = ["Christian", 1.74];
+var tupla_estudiante = ['Juan', 1.58];
+console.log(tupla_estudiante[0] + " mide " + tupla_estudiante[1] + " mts");
---
+++ Seccion2/Tupla.ts
@@ -0,0 +1,15 @@
+let vector: [number, number];
+let vector2: [number, number];
+
+vector=[2,5];
+vector2=[5,10];
+
+let vector_resultante: [number, number] = [vector[0]+vector2[0],vector[1]+vector2[1] ];
+
+console.log(vector_resultante);
+
+let tupla_5_elementos: [number, number, number, number, number]=[1,2,3,4,5];
+let tupla_profesor: [string, number] =["Christian", 1.74];
+let tupla_estudiante: [string, number]= ['Juan', 1.58];
+
+console.log(`${tupla_estudiante[0]} mide ${tupla_estudiante[1]} mts`);
