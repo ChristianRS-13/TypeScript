e080645fdcb9daf22b9f933a63ef59286869cb8b
Rosinet <christian.romero@rosinet.com.mx>
2020-01-25 23:54:30 -0600

    Declaracion de Numeros

---
+++ Seccion2/Number.js
@@ -0,0 +1,9 @@
+var mi_edad = 30;
+var mi_nota = 88.6;
+var mi_nota2 = 91.1;
+// mi_edad=true;
+console.log(mi_nota + mi_nota2);
+console.log(mi_nota - mi_nota2);
+console.log(mi_nota * mi_nota2);
+console.log(mi_nota / mi_nota2);
+console.log(mi_nota % mi_nota2);
---
+++ Seccion2/Number.ts
@@ -0,0 +1,11 @@
+let mi_edad: number = 30;
+let mi_nota: number = 88.6;
+let mi_nota2: number = 91.1;
+
+// mi_edad=true;
+
+console.log(mi_nota + mi_nota2);
+console.log(mi_nota - mi_nota2);
+console.log(mi_nota * mi_nota2);
+console.log(mi_nota / mi_nota2);
+console.log(mi_nota % mi_nota2);
